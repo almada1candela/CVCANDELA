d3ebf77065460f723c750437859060b56cecc60f
candela almada <cande_817@hotmail.com>
2023-02-19 22:36:12 -0300

    primer escritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById('boton_color').addEventListener('click', function(){
+    document.body.style.backgroundColor='#A3F5E4';
+    
+});
+
+document.getElementById('boton_default').addEventListener('click', function(){
+    document.body.style.backgroundColor='#FFF8F0';
+});
